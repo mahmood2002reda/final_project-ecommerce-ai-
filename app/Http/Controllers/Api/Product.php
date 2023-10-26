e6343a95811f8ddc3d90b20e5f49fd3744f4e366
mahmood2002reda <111697516+mahmood2002reda@users.noreply.github.com>
2023-10-26 19:14:21 +0300

    add change

--- app/Http/Controllers/Api/Product.php
+++ app/Http/Controllers/Api/Product.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class Product extends Controller
 {
-    //
+    public function index()
+    {
+        $books=User::get();
+       
+        return view('book.index',compact('books'));
+    }
 }
